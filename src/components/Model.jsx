cd52e7f75897362096491e5ce6913e841de042fa
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-17 17:22:28 +0530

    Add animations, Model and ModelView

---
+++ src/components/Model.jsx
@@ -0,0 +1,83 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useState, useRef, useEffect } from "react";
+import { yellowImg } from "../utils";
+import * as THREE from "three";
+import { animateWithGsapTimeline } from "../utils/animations";
+
+const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    colors: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    img: yellowImg,
+  });
+
+  // camera control for the model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  // model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  // rotation
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if (size === "large") {
+      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
+    }
+
+    if (size === "small") {
+      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
+    }
+  }, [size]);
+
+  useGSAP(() => {
+    gsap.to("#heading", { y: 0, opacity: 1 });
+  }, []);
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a close look.
+        </h1>
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={model}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Model;
