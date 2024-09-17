63198cb508fba6cfae697f0c6f013c00da83fb99
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-17 22:58:08 +0530

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <main className="bg-black">
@@ -13,6 +14,7 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   );
 };
