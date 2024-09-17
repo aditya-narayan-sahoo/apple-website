391bb7de6c783759045c18180c476ad3efe3396b
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-17 23:01:11 +0530

    Refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,18 @@ const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
-        <img src={appleImg} alt="Apple" width={14} height={18} />
+        <img
+          src={appleImg}
+          alt="Apple"
+          width={21}
+          height={18}
+          className="-mt-2"
+        />
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
             <div
               key={nav}
-              className="px-5 text-lg text-gray cursor-pointer hover:text-white transition-all"
+              className="px-5 text-xl text-gray cursor-pointer hover:text-white transition-all"
             >
               {nav}
             </div>
