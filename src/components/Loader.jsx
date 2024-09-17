bae0494ceef1fd88073bfc28b1f95de57f5c10f5
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-17 22:42:51 +0530

    Update Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,8 +4,13 @@ import React from "react";
 const Loader = () => {
   return (
     <Html>
-      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        <div className="w-[10vw] h-[10vw] rounded-full">Loading...</div>
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+        <div className="flex flex-col items-center">
+          <div className="loader w-[10vw] h-[10vw] rounded-full border-4 border-t-4 border-t-blue-500 border-gray-300 animate-spin"></div>
+          <span className="mt-4 text-white text-lg font-semibold">
+            Loading...
+          </span>
+        </div>
       </div>
     </Html>
   );
